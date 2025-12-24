9c62265f6a1de01074d3e280d297e1f8ada0b831
Rakhymzhan01 <Rakhymzhan0001@gmail.com>
2025-12-24 14:20:18 +0500

    added gifs to the cards

--- constants.ts
+++ constants.ts
@@ -6,7 +6,7 @@ export const TOOL_DEFINITIONS = [
     id: 'generate-video',
     titleKey: 'generateVideo.title',
     descriptionKey: 'generateVideo.description',
-    gifSrc: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd25ocmNnNmQ4M3VqM2VpMjZ1ZzJ2M2JtNnp2bWJjNHp0MDh4ZXEydyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oEjI5VtIhHvK37WYo/giphy.gif',
+    gifSrc: '/farm-life-ai.gif',
     tintClass: 'group-hover:border-cyan-400',
     shadowClass: 'group-hover:shadow-cyan-500/40'
   },
@@ -14,7 +14,7 @@ export const TOOL_DEFINITIONS = [
     id: 'talk-picture',
     titleKey: 'talkPicture.title',
     descriptionKey: 'talkPicture.description',
-    gifSrc: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3Y5emJubnhncjV3a283eXBqaWhqZTVwZXAwbDZ6dDJ2czJ4enVuaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3oKIPuM1xeV9ZrmV8Y/giphy.gif',
+    gifSrc: '/Art Girl GIF by Anne Horel.gif',
     tintClass: 'group-hover:border-purple-400',
     shadowClass: 'group-hover:shadow-purple-500/40'
   },
@@ -22,7 +22,7 @@ export const TOOL_DEFINITIONS = [
     id: 'serious-video',
     titleKey: 'seriousVideo.title',
     descriptionKey: 'seriousVideo.description',
-    gifSrc: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExbXN5ODl2Z3prOXlxdXB6aW03M3E4ZnhwbmF3Z2txZW8yMG9tcG5weSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7btPCcdNni12S_5e/giphy.gif',
+    gifSrc: '/To The Moon Office GIF by Doge Pound.gif',
     tintClass: 'group-hover:border-red-400',
     shadowClass: 'group-hover:shadow-red-500/40'
   },
@@ -30,7 +30,7 @@ export const TOOL_DEFINITIONS = [
     id: 'tell-story',
     titleKey: 'tellStory.title',
     descriptionKey: 'tellStory.description',
-    gifSrc: 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDR4ODZocHFwNTI5bW1mb3cydW9vbjZlM3Z0ajhjcWxhNTB2ZnN6eSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7aD4grHwn875T7zi/giphy.gif',
+    gifSrc: '/Teacher Explaining GIF.gif',
     tintClass: 'group-hover:border-green-400',
     shadowClass: 'group-hover:shadow-green-500/40'
   }
